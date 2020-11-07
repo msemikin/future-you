3d6eea1795c36f0ee2f70301e0579f85ba53cb5c
Eerik Muuli <eerik.muuli@stacc.ee>
2020-11-07 20:48:11 +0200

    Fixed image upload, stores in session now as well

--- junction2020/settings.py
+++ junction2020/settings.py
@@ -113,3 +113,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    # BASE_DIR / "static",
+    '/home/eerik/workspace/future-you/futureyou/static/',
+]
--- junction2020/urls.py
+++ junction2020/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
+
 from django.contrib import admin
 from django.urls import path
 from futureyou import views
@@ -27,5 +30,5 @@ urlpatterns = [
 
     # Language url
     url(r'^lang/(?P<lang>[e]{1}[nt]{1})/$', views.set_language, name='switchlanguage')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
